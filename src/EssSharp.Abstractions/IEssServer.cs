a4bf94304110f930dcb316726198033d7272baa9
Jason Jones <jasonwjones@gmail.com>
2023-01-10 11:30:33 -0800

    Sync impl of GetApplication

--- src/EssSharp.Abstractions/IEssServer.cs
+++ src/EssSharp.Abstractions/IEssServer.cs
@@ -14,6 +14,13 @@ namespace EssSharp
         public List<IEssApplication> GetApplications();
 
         /// <summary>
+        /// Gets the application with the given name.
+        /// </summary>
+        /// <param name="applicationName"></param>
+        /// <returns>An application object.</returns>
+        public IEssApplication GetApplication(string applicationName);
+        
+        /// <summary>
         /// Gets the list of applications for this server available to the connected user.
         /// </summary>
         /// <param name="cancellationToken" />
@@ -26,8 +33,7 @@ namespace EssSharp
         /// <param name="cancellationToken" />
         /// <returns>A list of <see cref="IEssVariable"/> objects.</returns>
         public Task<List<IEssVariable>> GetVariablesAsync( CancellationToken cancellationToken = default );
-
-
+        
         /// <summary>
         /// Gets the about information of this server
         /// </summary>
