4bed3d1e6167944353822da2f96225f08aa0a739
MatthewBAllen <matthew@appliedolap.com>
2023-05-16 12:43:08 -0500

    close #23 implementing I/EssUser interface and class
    Added I/EssUser interface and class

---
+++ src/EssSharp.Abstractions/IEssUser.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace EssSharp
+{
+    /// <summary />
+    public interface IEssUser : IEssObject
+    {
+        /// <summary>
+        /// Return the server associated with the user
+        /// </summary>
+        public IEssServer Server { get; }
+
+        /// <summary>
+        /// Return the display name
+        /// </summary>
+        public string DisplayName { get; }
+
+        /// <summary>
+        /// Return the user email
+        /// </summary>
+        public string Email { get; }
+    }
+}
---
+++ src/EssSharp/EssUser.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+using EssSharp.Model;
+
+namespace EssSharp
+{
+    /// <summary />
+    public class EssUser : EssObject, IEssUser
+    {
+        #region Private Data
+
+        private readonly EssServer  _server;
+        private readonly UserBean _user;
+
+        #endregion
+
+        #region Constructors
+
+        /// <summary />
+        internal EssUser( UserBean user, EssServer server ) : base(server?.Configuration, server?.Client)
+        {
+            _user = user ?? 
+                throw new ArgumentNullException(nameof(user), $"An API model {nameof(user)} is required to create an {nameof(EssUser)}.");
+
+            _server = server ??
+                throw new ArgumentNullException(nameof(server), $"An {nameof(EssServer)} {nameof(server)} is required to create an {nameof(EssUser)}.");
+        }
+
+        #endregion
+
+        #region IEssObject Members
+
+        /// <inheritdoc />
+        public override string Name  => _user?.Id;
+
+        /// <inheritdoc />
+        public override EssType Type => EssType.User;
+
+        #endregion
+
+        #region IEssUser Properties
+
+        /// <inheritdoc />
+        public IEssServer Server => _server;
+
+        /// <inheritdoc />
+        public string DisplayName => _user?.Name;
+
+        /// <inheritdoc />
+        public string Email => _user?.Email;
+
+        #endregion
+
+        #region IEssUser Members
+
+
+        #endregion
+    }
+}
