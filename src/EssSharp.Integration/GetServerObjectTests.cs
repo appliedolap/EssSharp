6d948201c93dbed178e6d72dd43f12905c90d612
Billy Booth <billy@appliedolap.com>
2023-07-11 16:25:29 -0500

    Integration Tests: Get application configurations

--- src/EssSharp.Integration/GetServerObjectTests.cs
+++ src/EssSharp.Integration/GetServerObjectTests.cs
@@ -9,7 +9,21 @@ namespace EssSharp.Integration
     [Collection("EssSharp Integration Tests"), Trait("type", "get"), CollectionPriority(5)]
     public class GetServerObjectTests : IntegrationTestBase
     {
-        [Fact(DisplayName = $@"GetServerObjectTests - 01 - Essbase_AfterLockCreation_CanGetLocks"), Priority(01)]
+        [Fact(DisplayName = $@"GetServerObjectTests - 01 - Essbase_AfterAppCreation_CanGetAppConfigurations"), Priority(01)]
+        public async Task Essbase_AfterAppCreation_CanGetAppConfigurations()
+        {
+            // Get an unconnected server.
+            var server = GetEssServer();
+
+            // Get the configurations for the sample application.
+            var configurations = await (await server.GetApplicationAsync("Sample"))
+                .GetConfigurationsAsync();
+
+            // Assert that the collection of configurations is not empty.
+            Assert.NotEmpty(configurations);
+        }
+
+        [Fact(DisplayName = $@"GetServerObjectTests - 02 - Essbase_AfterLockCreation_CanGetLocks"), Priority(02)]
         public async Task Essbase_AfterLockCreation_CanGetLocks()
         {
             // Get an unconnected server.
